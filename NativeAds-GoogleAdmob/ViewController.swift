16e7060af28adc18065343fced732b718347f775
H0neyHunter <usysstr@gmail.com>
2023-10-26 16:38:51 +0300

    Initial Commit

---
+++ NativeAds-GoogleAdmob/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NativeAds-GoogleAdmob
+//
+//  Created by Ümit Örs on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
